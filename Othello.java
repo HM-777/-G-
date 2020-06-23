7d08709ed7f0a4ad6880c3a9b692cd1bf956e498
Hortensia333 <56621229+Hortensia333@users.noreply.github.com>
2020-06-23 16:13:58 +0900

    Add files via upload

--- Othello.java
+++ Othello.java
@@ -2,12 +2,26 @@ public class Othello {
 	private int row = 8;	//オセロ盤の縦横マス数(2の倍数のみ)
 	// String [] grids = new String [row * row]; //局面情報
 	private int [][] grids = new int [row][row];
-	
-	private int turn; //手番
-private int empty=0;
-private int placeable=2;
-private boolean placeableflag=false;
-	// コンストラクタ
+
+	private int turn; //手番の色
+
+    private int empty=0;//盤面が空である
+    private int placeable=2;//置くことが出来る
+    private boolean placeableflag=false;
+
+    //パス用変数
+    private boolean pass_flag=false;
+
+    private int pass_count=0;
+
+    //黒い駒の数集計用
+    private  int cnt_black = 0;
+    //白い駒の数集計用
+    private  int cnt_white = 0;
+
+
+
+    // コンストラクタ
 	public Othello(){
 		turn = -1; //黒が先手
 		for(int x=0;x<row;x++) {
@@ -23,8 +37,25 @@ private boolean placeableflag=false;
 	}
 
 	// メソッド
-	public String checkWinner(){	// 勝敗を判断
-		return "black";
+	public int checkWinner(){	// 勝敗を判断
+		int winner=10;//初期値　この値が返った場合試合が終わっていない
+		if(isGameover()==false) {
+			if(cnt_black==cnt_white) {
+				winner=0;// draw
+
+			}else if(cnt_black>cnt_white) {
+				winner=-1; //黒勝ち
+			}else if(cnt_black<cnt_white) {
+				winner=1;//白勝ち
+			}
+		}
+		return winner;
+	}
+	public int getBlackstone() {//黒石の数を取得
+		return cnt_black;
+	}
+	public int getWhitestone() {//白石の数を取得
+		return cnt_white;
 	}
 	public int getTurn(){ // 手番情報を取得
 		return turn;
@@ -36,17 +67,51 @@ private boolean placeableflag=false;
 	turn*=-1;
 	}
 	public boolean isGameover(){	// 対局終了を判断
-		return true;
+
+		//まだ空いている座標があるかのフラグ
+	    boolean existempty = false;
+
+
+	    for(int i=0;i<8;i++) {
+	    	for(int j=0;j<8;j++) {
+	    		if(grids[i][j]==empty) {
+	    			existempty=true;
+	    		}else if(grids[i][j]==-1) {
+	    			cnt_black++;
+	    		}else if(grids[i][j]==1) {
+	    			cnt_white++;
+	    		}
+	    	}
+	    }
+
+  if(cnt_black==0||cnt_white==0) {//一色で染まった場合終了
+	  existempty=false;
+  }
+
+  if(pass_count>1) {//パス二回で終了
+	  existempty=false;
+  }
+
+
+
+		return existempty;
 	}
-	
+
 	public int getRow(){ //縦横のマス数を取得
 		return row;
 	}
+    public void undo() {
 
-	public void checkPlaceable() {
-		
+
+
+    }
+	public void checkPlaceable() {//おける場所探索
+pass_flag=true;
+
 		for(int i=0;i<row;i++) {
 			for(int j=0;j<row;j++) {
+				//空の盤面のみチェックする
+				if(grids[i][j]==empty) {
 			    turnLeftUp(i, j,false);
 			    turnUp(i, j,false);
 			    turnRightUp(i, j,false);
@@ -55,17 +120,24 @@ private boolean placeableflag=false;
 			    turnLeftDown(i, j,false);
 			    turnDown(i, j,false);
 			    turnRightDown(i, j,false);
-			    
-			    if(placeableflag==true) {
+
+			    if(placeableflag==true) {//もしおける場所が見つかったならおける場所変数を入れてパスフラグを折りパスカウントをリセット
+			    	pass_flag=false;
 			    	grids[i][j]=placeable;
+			    	pass_count=0;
 			    	placeableflag=false;
 			    }
+				}
 			}
+		}
+		if(pass_flag==true) {
+			pass_count++;
+			changeTurn();//パスフラグがtrueのままならパスカウントを増加させて手番変更
 		}
 	}
  public void setStone(int x, int y) {
 
-   
+
 
     // 駒を配置できる場合
     if (grids[y][x]==placeable) {
@@ -75,7 +147,7 @@ private boolean placeableflag=false;
       turnStone(x, y);
 
       changeTurn();
-    } 
+    }
 
   }
 
@@ -94,7 +166,7 @@ private boolean placeableflag=false;
 
   }
 
-   
+
    //flagは盤面に操作を反映する場合にはtrueしない場合はfalse
   public void turnLeftUp(int x, int y,boolean flag) {
     if (y > 1 && x > 1) {
