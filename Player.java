bfc55adbb2cb7ed8b61df85dcc218b8c8596c7bb
tommyboy5868 <48838532+tommyboy5868@users.noreply.github.com>
2020-07-13 13:54:20 +0900

    Add files via upload

--- Player.java
+++ Player.java
@@ -5,6 +5,8 @@ public class Player {
     private String myID = "";  //プレイヤーID
     private String myPassword = "";  //パスワード
     private int myRecord[] = null;  //戦績情報
+    private int myPlayerNo;
+    private int opponentPlayerNo;
 
     public Player(String NAME,String ID, String PASSWORD) {  //コンストラクタ
     	this.myName = NAME;
@@ -42,6 +44,22 @@ public class Player {
     }
     public int[] getRecord() {  //戦績情報取得
     	return myRecord;
+    }
+
+    public void setMyPlayerNo(int i) {
+    	myPlayerNo = i;
+    }
+
+    public int getMyPlayerNo() {
+    	return myPlayerNo;
+    }
+
+    public void setOpponentPlayerNo(int i) {
+    	opponentPlayerNo = i;
+    }
+
+    public int getOpponentPlayerNo() {
+    	return opponentPlayerNo;
     }
 
 }
