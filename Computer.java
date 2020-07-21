62960ea3025f9541d05e30307337951ed2b21358
Genki Sakata <genki@GenkinoMacBook-Air.local>
2020-07-21 14:54:30 +0900

    開放度(β)追加

--- Computer.java
+++ Computer.java
@@ -30,6 +30,7 @@ public class Computer {
 	//int phase = 0;	//phase:序盤〜終盤に分割して思考を変える．使わないかも
 	Random random = new Random();	//random:乱数生成器
 	Othello othello = new Othello(0);	//othello:Othelloクラスのメソッドを利用
+	int kaihoudo[] = new int[7];
 
 	//コンストラクタ
 	public Computer(int level, int color){
@@ -69,12 +70,14 @@ public class Computer {
 		 *beta:βカットする値*/
 	//あまりに時間がかかるなら打ち切り？
 	public int alphabeta(int turn, int depth, int alpha, int beta) {
-		int value ;	//(多分)ノードの評価値
+		int value ;	//ノードの評価値
 		int childValue;
 		int X = 100, Y = 100;	//評価の高い場所
+		//int kaihoudo;
 		//末端ノードなら盤面評価値を返す
 		if(depth == 0) {
-			return evaluateBoard(turn);
+			//System.out.println(evaluateBoard(turn));
+			return evaluateBoard(turn) + 2*kaihoudo[0];
 		}
 		// 自分のターンなら最小値，相手のターンなら最大値をとりあえず設定
 		if(turn == color) {
@@ -82,15 +85,15 @@ public class Computer {
 		}else{
 			value = Integer.MAX_VALUE;
 		}
-		//置ける場所(2)を探す.パスのときが考慮されているか検討
+		//置ける場所(2)を探す
 		for(int y = 0; y < 8; y++) {
 			for(int x = 0; x < 8; x++) {
 				if(othello.grids[y][x] == 2) {
 					//試しに打つ
 					othello.setStone(x, y);
+					kaihoudo[depth-1] = evaluateKaihoudo(turn);
 					othello.checkPlaceable();
 					//手番を変える(現在はsetStoneにchangeTurnが内包)
-
 					//次の評価値を求める
 					childValue = alphabeta(-turn, depth-1, alpha, beta);
 					//末端ノードまで来るか，子ノードの評価値が計算されているなら以下の処理へ
@@ -98,22 +101,12 @@ public class Computer {
 						//バグってたらこの辺怪しい
 					if(turn == color) {
 						if(childValue >= value) {
-							//評価値がvalueと等しければ，1/2の確率で更新
-							//if(childValue == value) {
-								//if(random.nextInt(2) == 0) {
-									//value = childValue;
-									//alpha = value;
-									//X = x;
-									//Y = y;
-								//}
 							//評価値がvalueより大きければ更新
-							//}else {
 							value = childValue;
 							alpha = value;
 							X = x;
 							Y = y;
 							}
-						//}
 						if(value > beta) {	//βカット
 							othello.undo();
 							return value;
@@ -121,22 +114,12 @@ public class Computer {
 					//相手のターンなら子ノードから最小値を選択(自分にとって不都合な手を指す)
 					}else{
 						if(childValue <= value) {
-							//評価値がvalueと等しければ，1/2の確率で更新
-							//if(childValue == value) {
-								//if(random.nextInt(2) == 0) {
-									//value = childValue;
-									//beta = value;
-									//X = x;
-									//Y = y;
-								//}
 								//評価値がvalueより小さければ更新
-							//}else {
 								value = childValue;
 								beta = value;
 								X = x;
 								Y = y;
 							}
-						//}
 						if(value < alpha) {	//αカット
 							othello.undo();
 							return value;
@@ -151,7 +134,7 @@ public class Computer {
 			return 10*Y + X; //1つの値で返す必要があるのでこの形に．Xは(戻り値)%10,Yは((戻り値)-(戻り値)%10)/10で復元できる．
 		//それ以外はノードの評価値を返す
 		}else {
-			return value;
+			return value + 2*kaihoudo[depth-1];
 		}
 	}
 
@@ -245,7 +228,7 @@ public class Computer {
 		for(int y = 0; y < 8; y++) {
 			for(int x = 0; x < 8; x++) {
 				if(othello.grids[y][x] == turn){
-					evaluation += evaluationMap[y][x];	//開放度を計算して，その値をかけてみる？
+					evaluation += evaluationMap[y][x];
 				}else if(othello.grids[y][x] == -turn) {
 					evaluation -= evaluationMap[y][x];
 				}
@@ -253,35 +236,48 @@ public class Computer {
 		}
 		return evaluation;
 	}
-
-	//Clientに呼び出されるメソッド．戻り値は座標
-			//grids[][]:思考する盤面
-		public int think(int grids[][]) {
-			//現在の盤面をコピー
-			for(int y = 0; y < 8; y++) {
-				for(int x = 0; x < 8; x++) {
-					othello.grids[y][x] = grids[y][x];
-
-				}
-			}
-
-			if(level == 1) {
-				return random();
-			}else{
-				//残り14手から全探索開始
-				//!このif文は怪しい...
-				if(othello.getTurn() != color) {
-					System.out.println("CHANGE TURN!");
-					othello.changeTurn();
-				}
-				if(othello.getBlackstone() + othello.getWhitestone() >= 50) {
-					return exhaustiveSearch(color, level, Integer.MIN_VALUE, Integer.MAX_VALUE);
-				}
-				else{
-					return alphabeta(color, level, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	//
+	private int evaluateKaihoudo(int turn) {
+		int kaihoudo = 0;
+		for(int y = 0; y < 8; y++) {
+			for(int x = 0; x < 8; x++) {
+				if(othello.grids[y][x] == 5) {
+					kaihoudo++;
 				}
 			}
 		}
+		if(turn == color) {
+			return kaihoudo;
+		}else {
+			return -kaihoudo;
+		}
+	}
+
+	//Clientに呼び出されるメソッド．戻り値は座標
+			//grids[][]:思考する盤面
+	public int think(int grids[][]) {
+		//現在の盤面をコピー
+		for(int y = 0; y < 8; y++) {
+			for(int x = 0; x < 8; x++) {
+				othello.grids[y][x] = grids[y][x];
+			}
+		}
+		if(level == 1) {
+			return random();
+		}else{
+			//!このif文は怪しい...
+			if(othello.getTurn() != color) {
+				System.out.println("CHANGE TURN!");
+				othello.changeTurn();
+			}
+			//levelがhardなら残り14手から全探索開始
+			if(othello.getBlackstone() + othello.getWhitestone() >= 50 && level == 7) {
+				return exhaustiveSearch(color, level, Integer.MIN_VALUE, Integer.MAX_VALUE);
+			}else{
+				return alphabeta(color, level, Integer.MIN_VALUE, Integer.MAX_VALUE);
+			}
+		}
+	}
 
 
 
